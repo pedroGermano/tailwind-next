ec7ae1e221f73edf507a50f8338495c9464228ac
Pedro Germano <pedrogermano232@gmail.com>
2024-03-29 21:32:23 -0300

    feat: button style create

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 export default function Home() {
   return (
     <div className="p-8 bg-slate-900 text-slate-100 h-screen">
-      <h1 className="font-bold text-primary text-5xl">Hello Tailwind</h1>
+      <h1 className="text__title">Hello Tailwind</h1>
       <h2>Hello Tailwind</h2>
       <p>Hello Tailwind</p>
+      <button className="btn">Sign in</button>
     </div>
   );
 }
