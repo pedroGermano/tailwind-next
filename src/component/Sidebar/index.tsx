585708ceba1d29f9452e01c3dc7acd8c9a846e2b
Pedro Germano <pedrogermano232@gmail.com>
2024-03-30 21:07:09 -0300

    feat: component nav create

--- src/component/Sidebar/index.tsx
+++ src/component/Sidebar/index.tsx
@@ -6,7 +6,7 @@ export function Sidebar() {
     <aside className="space-y-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
 
-      <div className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 scroll-px-96 py-2 shadow-sm">
+      <div className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 scroll-px-20 py-2 shadow-sm">
         <Search className="h-5 w-5 text-zinc-500 " />
         <input
           className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
